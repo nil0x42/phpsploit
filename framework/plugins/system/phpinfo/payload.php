f411503cd42ef378c5556ce738c8ed10b6283dab
nil0x42 <nil0x42@gmail.com>
2013-03-28 17:35:24 +0100

    phpinfo plugin's payload made more readable

--- framework/plugins/system/phpinfo/payload.php
+++ framework/plugins/system/phpinfo/payload.php
@@ -4,20 +4,53 @@ function phpinfo_array($return=false){
     ob_start();
     phpinfo(-1);
     $pi = preg_replace(
-    array('#^.*<body>(.*)</body>.*$#ms', '#<h2>PHP License</h2>.*$#ms',
-    '#<h1>Configuration</h1>#',  "#\r?\n#", "#</(h1|h2|h3|tr)>#", '# +<#','#> +#',
-    "#[ \t]+#", '#&nbsp;#', '#  +#', '# class=".*?"#', '%&#039;%',
-    '#<tr>(?:.*?)" src="(?:.*?)=(.*?)" alt="PHP Logo" /></a>'
-    .'<h1>PHP Version (.*?)</h1>(?:\n+?)</td></tr>#',
-    '#<h1><a href="(?:.*?)\?=(.*?)">PHP Credits</a></h1>#',
-    '#<tr>(?:.*?)" src="(?:.*?)=(.*?)"(?:.*?)Zend Engine (.*?),(?:.*?)</tr>#',
-    "# +#", '#<tr>#', '#</tr>#','#<br />#','#Copyright#'),
-    array('$1', '', '', "", '</$1>' . "\n", ' <','> ', ' ', ' ', ' ', '', ' ',
-    '<h2>PHP Configuration</h2>'."\n".'<tr><td>PHP Version</td><td>$2</td></tr>'.
-    "\n".'<tr><td>PHP Egg</td><td>$1</td></tr>',
-    '<tr><td>PHP Credits Egg</td><td>$1</td></tr>',
-    '<tr><td>Zend Engine</td><td>$2</td></tr>' . "\n" .
-    '<tr><td>Zend Egg</td><td>$1</td></tr>', ' ', '%S%', '%E%',' ',' Copyright'),
+
+    array('#^.*<body>(.*)</body>.*$#ms',
+          '#<h2>PHP License</h2>.*$#ms',
+          '#<h1>Configuration</h1>#',
+          "#\r?\n#",
+          "#</(h1|h2|h3|tr)>#",
+          '# +<#',
+          '#> +#',
+          "#[ \t]+#",
+          '#&nbsp;#',
+          '#  +#',
+          '# class=".*?"#',
+          '%&#039;%',
+          '#<tr>(?:.*?)" src="(?:.*?)=(.*?)" alt="PHP Logo" /></a>'
+            .'<h1>PHP Version (.*?)</h1>(?:\n+?)</td></tr>#',
+          '#<h1><a href="(?:.*?)\?=(.*?)">PHP Credits</a></h1>#',
+          '#<tr>(?:.*?)" src="(?:.*?)=(.*?)"(?:.*?)'
+            .'Zend Engine (.*?),(?:.*?)</tr>#',
+          "# +#",
+          '#<tr>#',
+          '#</tr>#',
+          '#<br />#',
+          '#Copyright#'),
+
+    array('$1',
+          '',
+          '',
+          "",
+          '</$1>'."\n",
+          ' <',
+          '> ',
+          ' ',
+          ' ',
+          ' ',
+          '',
+          ' ',
+          '<h2>PHP Configuration</h2>'."\n".'<tr><td>PHP Version</td><td>$2'
+            .'</td></tr>'."\n".'<tr><td>PHP Egg</td><td>$1</td></tr>',
+          '<tr><td>PHP Credits Egg</td><td>$1</td></tr>',
+          '<tr><td>Zend Engine</td><td>$2</td></tr>'."\n"
+            .'<tr><td>Zend Egg</td><td>$1</td></tr>',
+          ' ',
+          '%S%',
+          '%E%',
+          ' ',
+          ' Copyright'),
+
     ob_get_clean());
 
     $sections = explode('<h2>', strip_tags($pi, '<h2><th><td>'));
