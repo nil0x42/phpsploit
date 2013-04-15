862321c161957d8fecc0555d1cce6355bb46661d
nil0x42 <nil0x42@gmail.com>
2013-04-15 21:43:02 +0200

    changed termcolor format from %{} to ~{}
    because it was badly complatible combined with the standard
    python2 % format that is already used on phpsploit

--- core/termcolor.py
+++ core/termcolor.py
@@ -1,4 +1,4 @@
-"""Cross-Platform terminal color manager.
+"""Terminal color and style manager. (made for PhpSploit)
 
 """
 
@@ -6,14 +6,16 @@ import re
 import colorama, colorama_patched
 
 
-ANSI_CODES = {"RESET"       : 0,
-              "BOLD"        : 1,
+ANSI_CODES = {# styling codes
+              "RESET"       : 0,
+              "BRIGHT"      : 1,
               "DIM"         : 2,
+              "ITALIC"      : 3,
               "UNDERLINE"   : 4,
               "BLINK"       : 5,
               "REVERT"      : 7,
               "NORMAL"      : 22,
-
+              # coloration codes
               "black"       : 30,
               "red"         : 31,
               "green"       : 32,
@@ -26,29 +28,40 @@ ANSI_CODES = {"RESET"       : 0,
 
 
 
-def draw(ansiCodes):
-    """Takes a comma separated list of color codes, and return
-    the corresponding ansi color codes.
+def draw(codes):
+    """Draw the given ansi color list (which may be a string or
+    SRE_MATCH object).
+    They must be comma separated, and each element is picked from
+    the ANSI_CODES dictionnary.
 
+    >>> termcolor.draw('DIM,red')
+    '\\x1b[2m\\x1b[31m'
     """
+    if not isinstance(codes, str):
+        try: codes = codes.group(1)
+        except: raise ValueError("Invalid color list: {}".format(codes))
     result = ""
-    for code in ansiCodes.split(','):
-        result += "\x1b[%dm" %ANSI_CODES[code]
-    return(result)
+    for code in codes.split(','):
+        try: result += "\x1b[%dm" %ANSI_CODES[code]
+        except: pass
+    return result
 
 
 def format(string):
-    """Takes a color formated string as argument, and return
-    it's ANSI colored version.
-
+    """Format the given string, converting ~{ANSI,CODES} sequences
+    with the draw() function.
+    >>> termcolor.format('~{DIM,green}colored string~{RESET}')
+    '\\x1b[2m\\x1b[32mcolored string\\x1b[0m'
     """
-    patterns = "\%\{([A-Za-z,]+?)\}"
-    return( re.sub(patterns, draw, string) )
+    patterns = "\~\{([A-Za-z,]+?)\}"
+    return re.sub(patterns, draw, string)
 
 
 def blank(string):
-    """Remove ANSI color codes from the given string"""
+    """Remove ANSI color/style codes from the given string.
+
+    >>> termcolor.blank('string \\x1b[2m\\x1b[32mcolor !\\x1b[0m')
+    'string color !'
+    """
     regex = "\x01?\x1b\[((?:\d|;)*)([a-zA-Z])\x02?"
-    return( re.sub(regex, "", string) )
-
-
+    return re.sub(regex, "", string)
