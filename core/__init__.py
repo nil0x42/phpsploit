a4d1f62f65906a1b087281b5fd0850dc0d05cbca
nil0x42 <nil0x42@gmail.com>
2013-05-02 17:39:10 +0200

    core now loads/inits user conf dir (core.userdir)

--- core/__init__.py
+++ core/__init__.py
@@ -5,14 +5,117 @@ Finally, it updates the main python modules path to link to itself,
 making any ./core/* libraries directly available through the
 python environment.
 
+vars:
+=====
+    basedir -> /path/to/phpsploit/
+    coredir -> /path/to/phpsploit/core/
+    userdir -> /home/user/.phpsploit/
+
 """
+# deps must be imported before anything else
+import deps, sys, os
 
-# Make dependencies available:
-import deps
-
-# set basedir (phpsploit's root, aka ./), and coredir (./core) vars, then
-# re bind first priority path (sys.path[0]) to coredir instead of basedir.
-import sys, os.path
-basedir = sys.path[0]
+# `basedir` -> 'phpsploit/' && `coredir` -> 'phpsploit/core/'
+basedir = os.path.truepath(sys.path[0])
 coredir = os.path.join(basedir, __name__)
+
+# Use `coredir` as new primary python path
 sys.path[0] = coredir
+
+############### USER CONFIG DIR ################
+import hashlib
+from datatypes import Path
+
+def filehash(path):
+    """PhpSploit file hash signature.
+    This function gives and md5sum of the file data, in a cross platform
+    way, independently of the newlines.
+
+    WARNING: The md5 hex digest returned is completely dirrefent of a
+    real file md5sum.
+
+    """
+    lines = open(path, 'r').read().splitlines()
+    hash = hashlib.md5( str(lines).encode('utf-8') )
+    return hash.hexdigest()
+
+# Define `userdir`, the phpsploit user config dir.
+def _get_userdir():
+    """Return the PhpSploit user directory.
+    The following try order is used:
+        0 - $XDG_CONFIG_HOME/phpsploit/ (only if env var exists)
+        1 - ~/.config/phpsploit/
+        2 - ~/.phpsploit/
+
+    If no one exists, an mkdir is tried for each one in the
+    same order than the previous. Mkdir is not recursive,
+    meaning that parent must already exist.
+
+    If no userdir can be determined, a ValueError concerning
+    last possible choice (~/.phpsploit/) is raised.
+
+    """
+    choices = ["~/.config/phpsploit", "~/.phpsploit"]
+    if os.environ.get("XDG_CONFIG_HOME"):
+        choices.insert(0, "$XDG_CONFIG_HOME/phpsploit")
+    choices = [os.path.truepath(c) for c in choices]
+
+    # if a dir from `choices` exists, just return it
+    for choice in choices:
+        try: return Path(choice, mode="edrw")()
+        except: pass
+
+    # if no one exists, try to create one from choices
+    for choice in choices:
+        try:
+            os.mkdir(choice)
+            return Path(choice, mode="edrw")()
+        except: pass
+
+    # it raises the apropriate ValueError
+    Path(choice, mode="edrw")
+
+def _fill_userdir(path):
+    """Add user configuration dir's default content.
+
+    The default user config directory can be found at:
+        ./framework/rc_template/
+
+    """
+
+    # this dict takes relpaths of default user config files as keys.
+    # the value is a list of hashs. If one of these user config files
+    # exists, it will not be overwritten, except if its current hash
+    # is the same than one of the file's hash, that represent default
+    # values.
+    #NOTE: in order to get filehash of a file, use:
+    # >>> import core
+    # >>> core.filehash('/file/path')
+    files = {"config"         : [],
+             "plugins/README" : []}
+
+    for filepath, defaults in files.items():
+        # get path of template file
+        template = Path(basedir, 'framework/rc_template', filepath)
+
+        # add current default file content's hash to hashs list
+        defaults.append(filehash(template))
+
+        # get `path` (absolute path), and its parent dir
+        path = os.path.truepath(userdir, filepath)
+        dirname = os.path.dirname(path)
+
+        # make sur file's parent directory exists
+        try: os.makedirs(dirname)
+        except: pass
+        Path(dirname, mode="edrw")
+
+        # if user manually changed a file, don't overwrite it:
+        if os.path.exists(path) and filehash(path) not in defaults:
+            continue
+
+        # write template data to file
+        open(path, 'w').write( template.read() )
+
+userdir = _get_userdir()
+_fill_userdir(userdir)
--- core/datatypes/Path.py
+++ core/datatypes/Path.py
@@ -6,13 +6,14 @@ class Path(str):
     Takes one or more string arguments, joinded as a single absolute
     file or directory path.
 
-    The optionnal keyword arg, `rights` may define some conditionnal
+    The optionnal keyword arg, `mode` may define some conditionnal
     path properties.
 
     A ValueError is raised if path do not exists, or if at least one
-    of the `rights` conditions are not True.
+    of the `mode` conditions are not True.
 
-    Available rights tags:
+    Available mode tags:
+        e: path exists (default value)
         f: is a file
         d: is a directory
         r: is readable
@@ -20,53 +21,50 @@ class Path(str):
         w: is writable
 
     Example:
-    >>> Path("./test", 'frw')
+    >>> Path("./test", 'efrw')
     "/home/user/test"
 
     """
-    def __new__(cls, *args, rights=''):
-        path = os.path.join(*args)
+    def __new__(cls, *args, mode='e'):
+        path = os.path.truepath(*args)
 
-        if isinstance(path, tuple):
-            path = os.path.join(*path)
+        def error(msg):
+            raise ValueError("«{}»: {}".format(path, msg))
 
-        path = os.path.expandvars( str(path) )
-        path = os.path.realpath( os.path.expanduser(path) )
-
-        error = lambda msg: raise ValueError("«{}»: {}".format(path, msg))
-
-        # default assertion: path MUST exist anyway
-        if not os.path.exists(path):
+        # exists
+        if 'e' in mode and not os.path.exists(path):
             error("No such file or directory")
-
-        # optionnal `rights` tags assertions:
-        if 'f' in rights and not os.path.isfile(path):
+        # is file
+        if 'f' in mode and not os.path.isfile(path):
             error("Is a directory")
-        elif 'd' in rights
+        # is directory
+        if os.path.isdir(path):
             path += os.sep
-            if not os.path.isdir(path):
-                error("Not a directory")
-
-        if 'x' in rights and not os.access(path, os.X_OK):
+        elif 'd' in mode:
+            error("Not a directory")
+        # is executable
+        if 'x' in mode and not os.access(path, os.X_OK):
             error("Not executable")
-        if 'r' in rights and not os.access(path, os.R_OK):
+        # is readable
+        if 'r' in mode and not os.access(path, os.R_OK):
             error("Not readable")
-        if 'w' in rights and not os.access(path, os.W_OK):
+        # is writable
+        if 'w' in mode and not os.access(path, os.W_OK):
             error("Not writable")
 
         return str.__new__(cls, path)
 
 
     def __raw_value(self):
-        return super(Path, self).__str__()
+        return os.path.realpath( str(self) )
 
 
     def __call__(self):
-        return self.__raw_value()
+        return os.path.realpath( str(self) )
 
 
     def __str__(self):
-        return self.__raw_value()
+        return super(Path, self).__str__()
 
 
     def read(self):
