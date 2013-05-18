cc6e2ca51be80060f9ffe02285952dcfcf4d828a
nil0x42 <nil0x42@gmail.com>
2013-05-18 13:30:25 +0200

    env vars are now loaded from their dedicated class
    the lib/core/session/environment.py's Environment() class object is a
    simple MetaDict() extension that provides read-only tagged
    variables policy, meaning that some environment variables will raise an
    error if their value is being changed by user.

---
+++ lib/core/session/environment.py
@@ -0,0 +1,46 @@
+import re
+from . import baseclass
+
+class Environment(baseclass.MetaDict):
+    """Environment Variables
+
+    Instanciate a dict() like object that stores PhpSploit
+    environment variables.
+
+    Unlike settings, env vars object works exactly the same way than
+    its parent (baseclass.MetaDict), excepting the fact that some
+    items (env vars) are tagged as read-only.
+    This behavior only aplies if the concerned variable already
+    exists.
+    In order to set a tagged variable's value, it must not
+    exist already.
+
+    Example:
+    >>> Env = Environment()
+    >>> Env.PLATFORM = "foo"
+    >>> Env.PLATFORM = "bar"
+    AttributeError: «PLATFORM» variable is read-only
+    >>> Env.PLATFORM
+    'foo'
+    >>> del Env.PLATFORM
+    >>> Env.PLATFORM = "bar"
+    >>> Env.PLATFORM
+    'bar'
+
+    """
+    readonly = ["ADDR", "HOST", "PHP_VERSION", "PATH_SEP",
+                "HTTP_SOFTWARE", "WEB_ROOT"]
+
+    def __init__(self, value={}, readonly=[]):
+        self.readonly += readonly
+        super().__init__(value)
+
+
+    def __setitem__(self, name, value):
+        if name in self.readonly and name in self.keys():
+            raise AttributeError("«{}» variable is read-only".format(name))
+        super().__setitem__(name, value)
+
+
+    def _isattr(self, name):
+        return re.match("^[A-Z][A-Z0-9_]+$", name)
