d35244a217e2cfd8f6e84291e8801de6be3b7665
nil0x42 <nil0x42@gmail.com>
2014-04-09 01:01:40 +0200

    done a little clean on ./phpsploit and net encapsulator

--- api/phpfiles/encapsulator.php
+++ api/phpfiles/encapsulator.php
@@ -1,21 +1,31 @@
-<?
+<?php
 
 ini_set('display_errors','1');
 ini_set('error_reporting',E_ALL^E_NOTICE);
 
-$Q=array();
-$R=array();
+$Q = array();
+$R = array();
 
-function error($name='',$arg1=False,$arg2=False,$arg3=False,$arg4=False){
-    $args=array($name);
-    if($arg1 !== False) $args[]=$arg1;
-    if($arg2 !== False) $args[]=$arg2;
-    if($arg3 !== False) $args[]=$arg3;
-    if($arg4 !== False) $args[]=$arg4;
-    return(array('__ERROR__' => $args));}
+function error($name='', $arg1=False, $arg2=False, $arg3=False, $arg4=False)
+{
+    $args = array($name);
 
-function PAYLOAD(){
-    %%PAYLOAD%%}
+    if($arg1 !== False)
+        $args[] = $arg1;
+    if($arg2 !== False)
+        $args[] = $arg2;
+    if($arg3 !== False)
+        $args[] = $arg3;
+    if($arg4 !== False)
+        $args[] = $arg4;
+
+    return(array('__ERROR__' => $args));
+}
+
+function PAYLOAD()
+{
+    %%PAYLOAD%%
+}
 
 $RESULT = PAYLOAD();
 
