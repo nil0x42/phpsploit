53ce9870a6b4920c4b0e65b02fcdbc697775d643
nil0x42 <nil0x42@gmail.com>
2014-08-16 20:45:05 +0200

    Comment out test.test-connected.sh script

--- test/test-connected.sh
+++ test/test-connected.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# This script eases some debugging tasks.
+# It runs a php server, writes a backdoor to it,
+# then it runs the phpsploit framework with this
+# backdoor.
+# It means that running this script runs a
+# phpsploit session that is connected to a backdoor.
+# When leaving the framework, the php server is killed,
+# and its root directory removed.
 
 srv_addr="127.0.0.1:`shuf -i 10000-65500 -n 1`"
 srv_webdir="/tmp/`uuidgen`/"
