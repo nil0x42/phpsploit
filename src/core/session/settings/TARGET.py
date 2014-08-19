3e624b41c0266c392b4732794b6ffae1553e2dbc
nil0x42 <nil0x42@gmail.com>
2014-08-19 07:59:36 +0200

    Add base TARGET implem in new settings manager

--- src/core/session/settings/TARGET.py
+++ src/core/session/settings/TARGET.py
@@ -1,2 +1,16 @@
-class TARGET:
-    pass
+"""Set target server's URL
+
+doc
+"""
+
+import objects
+import datatypes
+
+metatype = objects.settings.RandLineBuffer
+
+
+def setter(value):
+    if str(value).lower() in ["", "none"]:
+        return None
+    else:
+        return datatypes.Url(value)
