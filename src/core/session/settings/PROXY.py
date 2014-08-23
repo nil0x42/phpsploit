e10746ee43befbf0271c97f123e0b8a5a73498d9
nil0x42 <nil0x42@gmail.com>
2014-08-23 12:59:52 +0200

    Implement $PROXY new-style setting

--- src/core/session/settings/PROXY.py
+++ src/core/session/settings/PROXY.py
@@ -1,2 +1,24 @@
+import objects
+import datatypes
+
+
 class PROXY:
-    pass
+    """
+    The proxy to use for request encapsulation.
+
+    You can set a proxy in order to encapsulate
+    the whole phpsploit requests for furtivity
+    or network analysis purposes.
+
+    This setting supports HTTP, HTTPS, SOCKS4
+    and SOCKS5 proxy types.
+
+    PROXY SYNTAX: <SCHEME>://<ADDRESS>:<PORT>
+    """
+    type = objects.settings.RandLineBuffer
+
+    def setter(self, value):
+        return datatypes.Proxy(value)
+
+    def default_value(self):
+        return self.setter(None)
