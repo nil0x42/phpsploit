a778a467d5a23d9d7408879a55559f1234279747
nil0x42 <nil0x42@gmail.com>
2014-08-23 12:33:20 +0200

    Implement $CACHE_SIZE noe-style setting

--- src/core/session/settings/CACHE_SIZE.py
+++ src/core/session/settings/CACHE_SIZE.py
@@ -1,5 +1,28 @@
-class CACHE_SIZE:
-    """my help docstring
-    foo bar baz qux
-    """
+import objects
+import datatypes
 
+
+class CACHE_SIZE:
+    """
+    This core configuration setting limits the
+    session's maximum size.
+
+    While using the phpsploit framework, some usage
+    informations are stored, such as commands history.
+    Changing this limit ensures that the session, if
+    saved whith the 1session save` command will not
+    exceed the given size.
+
+    USE CASES:
+      * The `history` feature takes this setting into
+        account for limiting the number of saved
+        command lines.
+    """
+    type = objects.settings.MultiLineBuffer
+
+    def setter(self, value):
+        return datatypes.ByteSize(value)
+
+    def default_value(self):
+        raw_value = "1 MiB"
+        return self.setter(raw_value)
