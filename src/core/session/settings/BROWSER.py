a900f5145bd7f094c307a214bcfa489ec8ea46ff
nil0x42 <nil0x42@gmail.com>
2014-08-23 12:32:53 +0200

    Implement $BROWSER new-style setting

--- src/core/session/settings/BROWSER.py
+++ src/core/session/settings/BROWSER.py
@@ -1,2 +1,26 @@
+import objects
+import datatypes
+
+
 class BROWSER:
-    pass
+    """
+    User's prefered web browser.
+
+    This setting can be overridden if automatic
+    detection of system's web browser fails,
+    or if you want to use another one for
+    commands that needs to know what web browser
+    to use.
+
+    USE CASES:
+      * The `phpinfo` plugin's '--browser' option
+        uses this setting to display server's
+        phpinfo() output.
+    """
+    type = objects.settings.MultiLineBuffer
+
+    def setter(self, value):
+        return datatypes.WebBrowser(value)
+
+    def default_value(self):
+        return self.setter("")
