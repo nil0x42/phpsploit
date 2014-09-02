b59a18195338e405af9afed90b0bd3c28708e784
nil0x42 <nil0x42@gmail.com>
2014-09-02 06:50:50 +0200

    Implement $REQ_ZLIB_TRY_LIMIT new-style setting

--- src/core/session/settings/REQ_ZLIB_TRY_LIMIT.py
+++ src/core/session/settings/REQ_ZLIB_TRY_LIMIT.py
@@ -1,2 +1,28 @@
+import objects
+import datatypes
+
+
 class REQ_ZLIB_TRY_LIMIT:
-    pass
+    """
+    This setting adjusts from what payload size
+    the zlib compression feature is disabled.
+
+    The framework requests engine looks for the best
+    way to spread the payload with a minimum of http
+    requests when payloads are large.
+    Therefore, the compression feature is exponentially
+    cpu greedy, so it is a good practice to set
+    the limit according to the cpu capacity of your
+    machine.
+    """
+    type = objects.settings.RandLineBuffer
+
+    def setter(self, value):
+        value = datatypes.ByteSize(value)
+        if value < 1:
+            raise ValueError("must be a positive bytes number")
+        return value
+
+    def default_value(self):
+        raw_value = "20 MiB"
+        return self.setter(raw_value)
