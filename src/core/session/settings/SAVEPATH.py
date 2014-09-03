9e4cddfee0e5bcaa4b9e31bfbb476405649be382
nil0x42 <nil0x42@gmail.com>
2014-09-03 19:05:22 +0200

    Implement $SAVEPATH new-style setting

--- src/core/session/settings/SAVEPATH.py
+++ src/core/session/settings/SAVEPATH.py
@@ -1,2 +1,20 @@
+import tempfile
+
+import objects
+import datatypes
+
+
 class SAVEPATH:
-    pass
+    """
+    The default directory to use for saving and
+    loading phpsploit sessions when a filename
+    if given instead of a file path.
+    """
+    type = objects.settings.RandLineBuffer
+
+    def setter(self, value):
+        return datatypes.Path(value, mode="drw")
+
+    def default_value(self):
+        raw_value = tempfile.gettempdir()
+        return self.setter(raw_value)
