f15989d107b5112a3d82a03adfcb0196124d7c74
nil0x42 <nil0x42@gmail.com>
2014-09-04 07:58:21 +0200

    Implement $TARGET new-style setting

--- src/core/session/settings/TARGET.py
+++ src/core/session/settings/TARGET.py
@@ -1,16 +1,23 @@
-"""Set target server's URL
-
-doc
-"""
-
 import objects
 import datatypes
 
-metatype = objects.settings.RandLineBuffer
 
+class TARGET:
+    """
+    The web URI to use as remote target.
+    In order to run a remote exploitation session
+    the given URL should be backdoored.
 
-def setter(value):
-    if str(value).lower() in ["", "none"]:
+    NOTE: The backdoor to use can be retrieved by
+          running `exploit --get-backdoor` command.
+    """
+    type = objects.settings.RandLineBuffer
+
+    def setter(self, value):
+        if str(value).lower() in ["", "none"]:
+            return self.default_value()
+        else:
+            return datatypes.Url(value)
+
+    def default_value(self):
         return None
-    else:
-        return datatypes.Url(value)
