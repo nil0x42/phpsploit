d3ce888e3789765e6f3968e76e942209df1c8ad1
nil0x42 <nil0x42@gmail.com>
2014-09-06 12:27:56 +0200

    Implement $TMPPATH new-style setting

--- src/core/session/settings/TMPPATH.py
+++ src/core/session/settings/TMPPATH.py
@@ -1,2 +1,19 @@
+import tempfile
+
+import objects
+import datatypes
+
+
 class TMPPATH:
-    pass
+    """
+    Default directory to use for writing
+    temporary files.
+    """
+    type = objects.settings.RandLineBuffer
+
+    def setter(self, value):
+        return datatypes.Path(value, mode="drw")
+
+    def default_value(self, ):
+        raw_value = tempfile.gettempdir()
+        return self.setter(raw_value)
