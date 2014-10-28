310d60affc236297041dbec6d497ac901cb9f068
nil0x42 <nil0x42@gmail.com>
2014-10-28 08:36:38 +0100

    Add draft for phpsploit legacy session loader

---
+++ src/backwards/v1/session.py
@@ -0,0 +1,57 @@
+"""Load a phpsplpoit v1 session file.
+
+This is a backward compatibility component, wich
+allows loading phpsploit session files that where
+generated with a framework version < 2.1.4.
+
+"""
+
+import pickle
+from backwards.utils import rename_key
+
+
+def load(path):
+    file = open(path, 'rb')
+    data = pickle.load(file, encoding="latin1")
+
+    import pprint
+    pprint.pprint(data)
+
+    session = {}
+
+    # XXX # File OBJECT ##############################
+    session["File"] = None
+
+    # XXX # Alias OBJECT #############################
+    session["Alias"] = {}
+
+    # XXX # Cache OBJECT #############################
+    session["Cache"] = {}
+
+    # XXX # Hist OBJECT ##############################
+    session["Hist"] = {}
+
+    # XXX # Env OBJECT ###############################
+    session["Env"] = {}
+
+    # $EDITOR
+    rename_key(data["SETTINGS"], "TEXTEDITOR", "EDITOR")
+    # $HTTP_USER_AGENT
+    rename_key(data["SETTINGS"], "USERAGENT", "HTTP_USER_AGENT")
+    if data["SETTINGS"]["HTTP_USER_AGENT"] == "%%RAND_UA%%":
+        data["SETTINGS"]["HTTP_USER_AGENT"] = "%%DEFAULT%%"
+    # $PASSKEY
+    rename_key(data["SETTINGS"], "POSTVAR", "PASSKEY")
+    if "%%HASHKEY%%" in data["SETTINGS"]["PASSKEY"]:
+        tmp = data["SETTINGS"]["PASSKEY"]
+        tmp = tmp.replace("%%HASHKEY%%", data["ENV_HASH"])
+        data["SETTINGS"]["PASSKEY"] = tmp
+    # $BACKDOOR
+    if "%%POSTVAR%%" in data["SETTINGS"]["BACKDOOR"]:
+        tmp = data["SETTINGS"]["BACKDOOR"]
+        tmp = tmp.replace("%%POSTVAR%%", "%%PASSKEY%%")
+        data["SETTINGS"]["BACKDOOR"] = tmp
+
+    session["Conf"] = data["SETTINGS"]
+
+    return session
