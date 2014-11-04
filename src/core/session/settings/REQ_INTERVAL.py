222b3e33a2452b16d3556d0a2e83d0a1a0180e02
nil0x42 <nil0x42@gmail.com>
2014-11-04 09:03:04 +0100

    Rewrite $REQ_INTERVAL setting's help docstring.

--- src/core/session/settings/REQ_INTERVAL.py
+++ src/core/session/settings/REQ_INTERVAL.py
@@ -1,20 +1,31 @@
 """
-The interval in seconds to sleep between two
-http requests.
+Interval (in seconds) to sleep between
+consecutive HTTP requests.
 
-This request configuration setting is used
-while sending a multipart http requests in cases
-where the payload is larger than available space
-in a single request.
-Indeed, this setting forces the requests engine
-to sleep a random number of seconds between the
-given interval.
+This setting can be used to detemine how
+many seconds the payload sender shall wait
+between two consecutive HTTP requests.
 
-Setting it to an high value can help furtivity
-while sending a very large payload, for example
-when you are uploading a very big file to the
-remote server whith the `upload` plugin.
+The option only affects large payload executions
+which must be sent within multiple HTTP requests
+(aka, multipart payloads), by picking up a randon
+interval to sleep between each of them.
+
+If a dash separated tuple of numbers is provided,
+a random value within given interval is randomly
+chosen each time the setting takes effect.
+The setting can also be set to a fixed value, in
+which case the multipart request handler will wait
+this exact number of seconds between each
+consecutive request.
+
+While sending very large payloads, such as through
+the 'upload' plugin, this setting can really
+improve furtivity by preventing the framework
+spamming the target server with many consecutive
+HTTP requests.
 """
+
 import objects
 import datatypes
 
