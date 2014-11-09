8f121b24aac64f1d539a121e4bf5f428f135841e
nil0x42 <nil0x42@gmail.com>
2014-11-09 01:08:37 +0100

    Simplified supidly complex encoding handler

--- src/core/encoding.py
+++ src/core/encoding.py
@@ -4,76 +4,38 @@ This small string encoding/decoding manager aims to provide
 an uniform encoding handling policy for the phpsploit framework.
 """
 
+import sys
 import codecs
 
 
-default_encoding = ""
-default_errors = ""
+try:
+    codecs.lookup("utf-8")
+    default_encoding = "utf-8"
+except LookupError:
+    default_encoding = sys.getdefaultencoding()
+
+try:
+    codecs.lookup_error("surrogateescape")
+    default_errors = "surrogateescape"
+except LookupError:
+    default_errors = "strict"
 
 
-def encode(str_obj, encoding="", errors=""):
+def encode(str_obj, encoding=default_encoding, errors=default_errors):
     """Encode the given bytes object with `encoding` encoder
     and `errors` error handler.
     If not set, `encoding` defaults to module's `default_encoding` variable.
     If not set, `errors` defaults to module's `default_errors` variable.
     """
-    if not encoding:
-        encoding = default_encoding
-    if not errors:
-        errors = default_errors
     bytes_obj = str_obj.decode(encoding, errors)
     return bytes_obj
 
 
-def decode(bytes_obj, encoding="", errors=""):
+def decode(bytes_obj, encoding=default_encoding, errors=default_errors):
     """Decode the given bytes object with `encoding` decoder
     and `errors` error handler.
     If not set, `encoding` defaults to module's `default_encoding` variable.
     If not set, `errors` defaults to module's `default_errors` variable.
     """
-    if not encoding:
-        encoding = default_encoding
-    if not errors:
-        errors = default_errors
     str_obj = bytes_obj.decode(encoding, errors)
     return str_obj
-
-
-def set_default_encoding(*args):
-    """Takes n candidates to be used as default encoding
-    and choose the first one which is available.
-    If no candidate is available, default_encoding is set to 'utf-8'.
-    """
-    global default_encoding
-    default_encoding = "utf-8"
-    for candidate in args:
-        try:
-            codecs.lookup(candidate)
-        except LookupError:
-            continue
-        default_encoding = candidate
-        break
-
-
-def set_default_errors(*args):
-    """Takes n candidates to be used as default error handlers
-    and choose the first one which is available.
-    If no candidate is available, default_errors is set to 'strict'.
-    """
-    global default_errors
-    default_errors = "strict"
-    for candidate in args:
-        try:
-            codecs.lookup_error(candidate)
-        except LookupError:
-            continue
-        default_errors = candidate
-        break
-
-
-# initialize default_encoding to 'utf-8'
-set_default_encoding("utf-8")
-
-# initialise default_errors to 'surrogateescape'
-# is not available, default to 'strict' error handler.
-set_default_errors("surrogateescape", "strict")
