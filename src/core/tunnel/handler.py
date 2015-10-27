b14536335b2839ee0d10dac66f11efc8c56b83e2
nil0x42 <nil0x42@gmail.com>
2015-10-27 17:39:41 +0100

    disable https certificate verification
    this (ugly) patch allows putting backdoor on https pages
    with invalid certificate

--- src/core/tunnel/handler.py
+++ src/core/tunnel/handler.py
@@ -17,6 +17,16 @@ from ui.color import colorize
 from .exceptions import BuildError, RequestError, ResponseError
 from . import payload
 
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 
 class Request:
 
