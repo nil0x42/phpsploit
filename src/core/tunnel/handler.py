b357be3c80bed23cb33d85d9208d751d80476ba7
nil0x42 <nil0x42@gmail.com>
2016-07-24 23:06:50 +0200

    trivial code review on tunnel handler

--- src/core/tunnel/handler.py
+++ src/core/tunnel/handler.py
@@ -21,14 +21,8 @@ from . import payload
 ### disable ssl signed certificates vetrification, in order to be
 ### able to use the phpsploit backdoor over non signed https targets.
 import ssl
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
+if hasattr(ssl, "_create_unverified_context"):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 ##############################################################################
 ### custom http connection handlers: save raw plaintext http/https requests
