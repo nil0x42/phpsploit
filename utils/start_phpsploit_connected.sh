02bbe9af6793ac2e6b0184a2ed148b74724d9e4e
nil0x42 <nil0x42@gmail.com>
2016-11-30 21:36:54 +0100

    Add utils script to test phpsploit connected
    It starts an http server with php -S

---
+++ utils/start_phpsploit_connected.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$(realpath `dirname $0`)"
+cd `git rev-parse --show-toplevel`
+
+SRV_ADDR="127.0.0.1:64956"
+SRV_WEBDIR="/tmp/phpsploit-temp-server/"
+
+# reset srv_webdir
+mkdir -p "$SRV_WEBDIR"
+
+# inject backdoor on server
+phpsploit -e 'set BACKDOOR %%DEFAULT%%; exploit --get-backdoor' > "$SRV_WEBDIR/index.php"
+
+# return phpsploit command
+echo "use the following command to connect phpsploit to server:"
+echo "    ----------------------------------------"
+echo "    phpsploit -t $SRV_ADDR -ie 'set BACKDOOR %%DEFAULT%%; set REQ_HEADER_PAYLOAD %%DEFAULT%%; exploit'"
+echo "    ----------------------------------------"
+
+# start server
+php -S "$SRV_ADDR" -t "$SRV_WEBDIR"
