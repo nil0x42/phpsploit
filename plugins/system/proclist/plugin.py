71a3b231a71a5441cb8df9e4a13d278721093273
jose nazario <jose.monkey.org@gmail.com>
2018-08-25 17:06:35 -0400

    plugins: add `proclist` plugin
    List current processes

---
+++ plugins/system/proclist/plugin.py
@@ -0,0 +1,21 @@
+import sys
+
+from api import plugin
+from api import server
+
+import plugin_args
+
+if len(plugin.argv) < 2:
+    sys.exit(plugin.help)
+
+opt = plugin_args.parse(plugin.argv[1:])
+
+payload = server.payload.Payload("proclist.php")
+
+result = payload.send()
+
+print("%5s %40s" % ("PID", "INFORMATION"))
+print("%5s %40s" % ("---", "-----------"))
+for elem in results:
+    pid, info = elem.split(" ", 1)
+    print("%5s %40s" % (pid, info.strip()))
