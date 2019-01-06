9bcf632f1c20c4eb8af662a635a9b4c0e9b4bcb6
nil0x42 <nil0x42@gmail.com>
2019-01-06 20:38:04 +0000

    Add release generator

---
+++ utils/make_release.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$(realpath `dirname $0`)"
+cd `git rev-parse --show-toplevel`
+
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m' 
+NC='\033[0m' # No Color
+
+
+
+########## ERROR HANDLING
+
+
+# check arguments
+if (( $# != 1 )); then
+    >&2 echo -e "Usage: $0 <version>"
+    >&2 echo -e "Example: ${GREEN}$0 3.0${NC}"
+    exit 1
+fi
+
+
+# check current branch is master
+cur_branch="`git rev-parse --abbrev-ref HEAD`"
+if [ "$cur_branch" != "master" ]; then
+    >&2 echo -e "\n${RED}[-] Please checkout on master branch:${NC}"
+    >&2 echo -e "    ${GREEN}git checkout master${NC}"
+    exit 1
+fi
+
+
+# check existence of `github_changelog_generator` tool
+gh_changelog_gen="github_changelog_generator"
+if ! which "$gh_changelog_gen" >/dev/null; then
+    >&2 echo -e "\n${RED}[-] Please install '$gh_changelog_gen':${NC}"
+    >&2 echo -e "    ${GREEN}sudo gem install ${gh_changelog_gen}${NC}"
+    exit 1
+fi
+
+
+# make sure that git work tree is clean
+# source: https://stackoverflow.com/questions/30063411/git-bash-script-check-working-tree
+require_clean_work_tree () {
+    git rev-parse --verify HEAD >/dev/null || exit 1
+    git update-index -q --ignore-submodules --refresh
+    err=0
+    if ! git diff-files --quiet --ignore-submodules
+    then
+        echo >&2 "Cannot $1: You have unstaged changes."
+        err=1
+    fi
+    if ! git diff-index --cached --quiet --ignore-submodules HEAD --
+    then
+        if [ $err = 0 ]
+        then
+            echo >&2 "Cannot $1: Your index contains uncommitted changes."
+        else
+            echo >&2 "Additionally, your index contains uncommitted changes."
+        fi
+        err=1
+    fi
+    if [ $err = 1 ]
+    then
+        test -n "$2" && echo >&2 "$2"
+        exit 1
+    fi
+}
+require_clean_work_tree
+if [ -n "`git status --porcelain`" ]; then
+    >&2 echo -e "\n${RED}[-] Please commit or stash untracked files${NC}"
+    >&2 echo -e "    ${GREEN}git status${NC}"
+    exit 1
+fi
+
+
+# check nothing to pull
+local_head="`git rev-parse HEAD`"
+remote_head="`git ls-remote origin master | cut -f1`"
+if [ "$local_head" != "$remote_head" ]; then
+    >&2 echo -e "\n${RED}[-] Local branch != remote, please pull or push:${NC}"
+    >&2 echo -e "    ${GREEN}git pull; git push${NC}"
+    exit 1
+fi
+
+
+
+########## MAKE RELEASE
+
+VER=$1
+TAG="v${VER}"
+MSG="Release version ${VER}"
+
+# set verbose/err
+set -ve
+
+# replace VERSION string in ./phpsploit
+sed -i '/^VERSION = /c\VERSION = "'${VER}'"' ./phpsploit
+git add ./phpsploit
+
+
+# generate CHANGELOG.md
+github_changelog_generator --future-release ${TAG}
+git add ./CHANGELOG.md
+
+# create commit
+git commit -m "$MSG"
+
+# add release tag to created commit
+git tag -a v3.0 -m "Version 3.0"
+
+# unset verbose/err
+set +ve
+
+echo -e "${BOLD}[+] Release commit created successfully${NC}"
+echo -e ""
+echo -e "  1. Check the release commit:"
+echo -e "      ${GREEN}git diff HEAD^${NC}"
+echo -e ""
+echo -e "  2. Push the commit and it's tag"
+echo -e "      git push --follow-tags"
