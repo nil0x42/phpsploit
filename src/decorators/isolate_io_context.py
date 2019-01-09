e6f60f9d9b7c624c9a5109979ffb5c1e50b84c01
nil0x42 <nil0x42@gmail.com>
2019-01-09 14:05:27 +0000

    Fix issue #54
    `isolate_io_context()` and `isolate_readline_context()` decorators
    now properly isolate readline's history.

--- src/decorators/isolate_io_context.py
+++ src/decorators/isolate_io_context.py
@@ -1,45 +1,58 @@
 """A decorator for separating I/O context.
-
-This decorator isolates I/O context of target
-function or method.
-
-I/O Context is a mix of terminal related elements,
-such as current stdout and readline completer
-attributes.
-
-This decorator is useful if you run something
-that reconfigures the readline completer, or
-needs to use the default stdout file descriptor
-instead of the phpsploit's stdout wrapper.
-
 """
-
 import sys
 
 
 def isolate_io_context(function):
+    """A decorator for separating I/O context.
+
+    This decorator isolates I/O context of target
+    function or method.
+
+    I/O Context is a mix of terminal related elements,
+    such as current stdout and readline completer
+    attributes.
+
+    This decorator is useful if you run something
+    that reconfigures the readline completer, or
+    needs to use the default stdout file descriptor
+    instead of the phpsploit's stdout wrapper.
+    """
     def wrapper(*args, **kwargs):
         try:
             import readline
             handle_readline = True
-        except:
+        except ImportError:
             handle_readline = False
 
-        # backup phpsploit I/O context
         if handle_readline:
+            # backup & reset readline completer
             old_readline_completer = readline.get_completer()
             readline.set_completer((lambda x: x))
+            # backup & reset readline history
+            old_readline_history = []
+            hist_sz = readline.get_current_history_length()
+            for i in range(1, hist_sz + 1):
+                line = readline.get_history_item(i)
+                old_readline_history.append(line)
+            readline.clear_history()
+        # backup & reset stdout
         old_stdout = sys.stdout
-        # function's stdout is the default one
         sys.stdout = sys.__stdout__
-        # execute function with fresh context
+
         try:
             retval = function(*args, **kwargs)
-        # restore phpsploit I/O context
         finally:
+
             if handle_readline:
+                # restore old readline completer
                 readline.set_completer(old_readline_completer)
+                # restore old readline history
+                readline.clear_history()
+                for line in old_readline_history:
+                    readline.add_history(line)
+            # restore old stdout
             sys.stdout = old_stdout
-        # return function result
+
         return retval
     return wrapper
--- src/decorators/isolate_readline_context.py
+++ src/decorators/isolate_readline_context.py
@@ -1,36 +1,50 @@
 """A decorator for separating readline context.
-
-This decorator isolates readline context of target
-function or method.
-
-Use when phpsploit's readline context should be
-reset temporarly is the context of triggering
-function or method.
-
-Unlike `isolate_io_context`, this decorator keeps
-original stdout wrapper.
 """
 
 
 def isolate_readline_context(function):
+    """A decorator for separating readline context.
+
+    This decorator isolates readline context of target
+    function or method.
+
+    Use when phpsploit's readline context should be
+    reset temporarly is the context of triggering
+    function or method.
+
+    Unlike `isolate_io_context`, this decorator keeps
+    original stdout wrapper.
+    """
     def wrapper(*args, **kwargs):
         try:
             import readline
             handle_readline = True
-        except:
+        except ImportError:
             handle_readline = False
 
-        # backup phpsploit I/O context
         if handle_readline:
+            # backup & reset readline completer
             old_readline_completer = readline.get_completer()
             readline.set_completer((lambda x: x))
-        # execute function with fresh context
+            # backup & reset readline history
+            old_readline_history = []
+            hist_sz = readline.get_current_history_length()
+            for i in range(1, hist_sz + 1):
+                line = readline.get_history_item(i)
+                old_readline_history.append(line)
+            readline.clear_history()
+
         try:
             retval = function(*args, **kwargs)
-        # restore phpsploit I/O context
         finally:
+
             if handle_readline:
+                # restore old readline completer
                 readline.set_completer(old_readline_completer)
-        # return function result
+                # restore old readline history
+                readline.clear_history()
+                for line in old_readline_history:
+                    readline.add_history(line)
+
         return retval
     return wrapper
