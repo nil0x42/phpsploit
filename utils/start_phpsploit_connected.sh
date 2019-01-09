cd93468902d327a2191d529b03759f4b09d0aece
nil0x42 <nil0x42@gmail.com>
2019-01-09 02:01:38 +0000

    start_phpsploit_connected.sh: fix cmd generator

--- utils/start_phpsploit_connected.sh
+++ utils/start_phpsploit_connected.sh
@@ -10,12 +10,12 @@ SRV_WEBDIR="/tmp/phpsploit-temp-server/"
 mkdir -p "$SRV_WEBDIR"
 
 # inject backdoor on server
-phpsploit -e 'set BACKDOOR %%DEFAULT%%; exploit --get-backdoor' > "$SRV_WEBDIR/index.php"
+./phpsploit -e 'set BACKDOOR %%DEFAULT%%; exploit --get-backdoor' > "$SRV_WEBDIR/index.php"
 
 # return phpsploit command
 echo "use the following command to connect phpsploit to server:"
 echo "    ----------------------------------------"
-echo "    phpsploit -t $SRV_ADDR -ie 'set BACKDOOR %%DEFAULT%%; set REQ_HEADER_PAYLOAD %%DEFAULT%%; exploit'"
+echo "    `pwd`/phpsploit -t $SRV_ADDR -ie 'set BACKDOOR %%DEFAULT%%; set REQ_HEADER_PAYLOAD %%DEFAULT%%; exploit'"
 echo "    ----------------------------------------"
 
 # start server
