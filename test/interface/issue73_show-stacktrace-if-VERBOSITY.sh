c837f6f1df72a9c83edb3898a31f1fa1f3fb8b83
nil0x42 <nil0x42@gmail.com>
2019-01-23 00:33:18 +0000

    test: update test framework

--- test/interface/issue73_show-stacktrace-if-VERBOSITY.sh
+++ test/interface/issue73_show-stacktrace-if-VERBOSITY.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Show stack trace when VERBOSITY is True
-#
-# Issue: #73
-# Ref: https://github.com/nil0x42/phpsploit/issues/73
+# Issue #73: Show stack trace when VERBOSITY is True
+# ref: https://github.com/nil0x42/phpsploit/issues/73
 
-stacktrace='Traceback (most recent call last):'
+stacktrace='Traceback (most recent call last)'
 
 # this SHOULD NOT contain stack trace:
-$PHPSPLOIT -e 'set VERBOSITY False; source /; lrun true' > $TMPFILE
-! grep "$stacktrace" $TMPFILE
+$PHPSPLOIT -e 'set VERBOSITY False; source /; true' > $TMPFILE
+grep -q "$stacktrace" $TMPFILE && exit 1
 
 # this SOULD contain stack trace:
-$PHPSPLOIT -e 'set VERBOSITY True; source /; lrun true' > $TMPFILE
-grep -q "$stacktrace" $TMPFILE
+$PHPSPLOIT -e 'set VERBOSITY True; source /; true' > $TMPFILE
+grep -q "$stacktrace" $TMPFILE || exit 1
