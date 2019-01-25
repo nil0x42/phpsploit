5393fb3af4533e945374f3eb2134f21d6a145794
nil0x42 <nil0x42@gmail.com>
2019-01-25 22:50:44 +0000

    test: add test for issue #70

--- test/core-commands/help.sh
+++ test/core-commands/help.sh
@@ -84,6 +84,16 @@ done
 
 
 ###
+### help <COMMAND> <ARG>
+###
+# issue #70: get help for `CMD` when calling `help CMD ARG`
+# Ref: https://github.com/nil0x42/phpsploit/issues/70
+# ensure this gives help for `corectl`:
+phpsploit_pipe help corectl FOOBAR > $TMPFILE || FAIL
+grep -q '^\[\*\] corectl: ' $TMPFILE || FAIL
+
+
+###
 ### Check invalid commands
 ###
 # check output
