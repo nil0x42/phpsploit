30518b752bdeee10cca3b643e3a2baad0cf318ac
nil0x42 <nil0x42@gmail.com>
2019-02-13 14:30:14 +0000

    CI/functest: improve functest FAIL's output

--- test/interface/tty-colors.sh
+++ test/interface/tty-colors.sh
@@ -29,12 +29,12 @@ grep -Pq '\033\[' $TMPFILE-2 || FAIL
 
 # assert file contains at least 500 ansi colors
 ansi_colors=$(perl -lne 'END {print $c} $c += s/\033\[//g' $TMPFILE-2)
-[ "$ansi_colors" -lt 500 ] && FAIL "got only $ansi_colors ansi colors"
+[ "$ansi_colors" -lt 500 ] && FAIL $ansi_colors
 
 # ensure `exploit --get-backdoor` gets colored by pygments
 ansi_colors=$(grep -v BACKDOOR $TMPFILE-2 | grep HTTP_PHPSPL01T \
     | perl -lne 'END {print $c} $c += s/\033\[//g')
-[ "$ansi_colors" -lt 9 ] && FAIL "got only $ansi_colors ansi colors"
+[ "$ansi_colors" -lt 9 ] && FAIL $ansi_colors
 
 # both should be equal after removing ansi colors
 decolorize $TMPFILE-2
