493fa2ed4d6cec6aa5403b118bc05dbfbad66fba
nil0x42 <nil0x42@gmail.com>
2019-02-25 17:40:16 +0000

    CI: update phpsploit pylint checker

--- utils/pylint.sh
+++ utils/pylint.sh
@@ -10,8 +10,9 @@ PYTHONPATH=":${src}:${deps}:${PYTHONPATH}"
 
 pre_args="--good-names=e,x,i,p,m --disable=no-member,not-callable"
 if [ -z "$1" ]; then
+    # default args (if not defined in argv[1:])
     args="$BASEDIR/phpsploit $BASEDIR/src/"
-    exec pylint3 $pre_args $args
+    exec /usr/bin/pylint3 $pre_args $args
 else
-    exec pylint3 $pre_args "$@"
+    exec /usr/bin/pylint3 $pre_args "$@"
 fi
