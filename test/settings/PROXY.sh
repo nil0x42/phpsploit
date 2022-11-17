0d30c6828f702584d253da0b546370326a35339a
nil0x42 <nil0x42@users.noreply.github.com>
2022-11-17 13:51:40 +0100

    ci/text: Add test for PROXY setting

---
+++ test/settings/PROXY.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+############################################################
+### VALID VALUES
+############################################################
+
+valid_schemes="http https socks4 socks4a socks5 socks5h"
+invalid_schemes="xxx httpx httpss socks socks4h socks5a"
+
+phpsploit_pipe set PROXY None || FAIL
+for scheme in $invalid_schemes; do
+    echo invalid_scheme=$scheme
+    phpsploit_pipe set PROXY "$scheme://0.0.0.0:23487" > $TMPFILE && FAIL
+    assert_contains $TMPFILE "Value Error: Invalid proxy format"
+    phpsploit_pipe exploit || FAIL
+    phpsploit_pipe exit || FAIL
+done
+
+for scheme in $valid_schemes; do
+    echo valid_scheme=$scheme
+    phpsploit_pipe set PROXY "$scheme://0.0.0.0:23487" > $TMPFILE || FAIL
+    assert_no_output $TMPFILE
+    phpsploit_pipe exploit > $TMPFILE && FAIL
+    if [[ "$scheme" == "socks4"* ]]; then
+        assert_contains $TMPFILE "Request error: Error connecting to SOCKS4 proxy "
+    elif [[ "$scheme" == "socks5"* ]]; then
+        assert_contains $TMPFILE "Request error: Error connecting to SOCKS5 proxy "
+    fi
+done
+
+# phpsploit_pipe set PROXY https://0.0.0.0:23487 > $TMPFILE || FAIL
+# assert_no_output $TMPFILE
+# phpsploit_pipe exploit && FAIL
+
+# phpsploit_pipe set PROXY None > $TMPFILE || FAIL
+# assert_no_output $TMPFILE
+# phpsploit_pipe exploit || FAIL
+# phpsploit_pipe exit
+
+# phpsploit_pipe help
